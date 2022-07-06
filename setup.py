384694d2f14316cf2d44bb5d9230cbe3c10f1075
Chazzcoin <chazzcoin@gmail.com>
2022-07-06 15:06:41 -0500

    automated changes

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ current = os.getcwd()
 
 setup(
     name='FairPICS',
-    version='2.0.1',
+    version='2.0.3',
     description='Source Provider & Topic/Category Analyzer.',
-    url='https://github.com/chazzcoin/Fopics',
+    url='https://github.com/chazzcoin/Fairpics',
     author='ChazzCoin',
     author_email='chazzcoin@gmail.com',
     license='BSD 2-clause',
@@ -15,7 +15,7 @@ setup(
     package_data={
         'fopResources': ['*.txt', '*.csv']
     },
-    install_requires=['FCoRE~=1.0.3'],
+    install_requires=['FCoRE~=1.0.3', 'pandas~=1.4.2'],
     classifiers=[
         'Development Status :: 1 - Planning',
         'Intended Audience :: Science/Research',
